fdcd89c0f60d1481c9234815914b0bbee13491e8
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-03-04 01:30:03 +0300

    update main page

--- js/index.js
+++ js/index.js
@@ -5,3 +5,21 @@ window.addEventListener("scroll", () => {
     ("window-scroll", window.scrollY > 0)
 })
 
+// show faq hidden paragraph answer
+const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("open")
+
+        const icon = faq.querySelector(".faq_icon i");
+        if (icon.className === 'bi bi-plus') {
+            icon.className = "bi bi-dash"
+        } else {
+            icon.className = 'bi bi-plus'
+        }
+    })
+})
+
+
+
